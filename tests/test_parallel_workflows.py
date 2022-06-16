3c59eb490b2b9ad99cbcf3f1ca32388aa9d7884d
chrishavlin <chris.havlin@gmail.com>
2022-06-16 15:42:29 -0500

    some parallel tests

--- tests/test_parallel_workflows.py
+++ tests/test_parallel_workflows.py
@@ -1,7 +1,9 @@
 import pytest
 import numpy as np
 from ci_sandbox import parallel_workflows
-
+import dask
+from dask.distributed.utils_test import gen_cluster, inc
+from dask.distributed import Client, Future, Scheduler, Worker
 
 def test_serial_read():
 
@@ -11,3 +13,42 @@ def test_serial_read():
     chunksize = int(1e4)
     data = parallel_workflows.serial_read(nchunks, chunksize, single_chunk)
     assert data.size == nchunks * chunksize
+
+
+def test_dask_embarrassing():
+    nchunks = 10
+    chunksize = int(1e4)
+
+    def single_chunk(array_size):
+        return np.random.random((array_size,),)
+
+    dask_dlay = []
+    for ichunk in range(nchunks):
+        chunk = dask.delayed(single_chunk)(chunksize)
+        dask_dlay.append(dask.delayed(np.min)(chunk))
+
+    minvals = dask.compute(*dask_dlay)
+    assert len(minvals) == nchunks
+
+
+
+
+# example from dask docs
+# https://distributed.dask.org/en/stable/develop.html#writing-tests
+@gen_cluster(client=True)
+async def test_submit(c, s, a, b):
+    assert isinstance(c, Client)
+    assert isinstance(s, Scheduler)
+    assert isinstance(a, Worker)
+    assert isinstance(b, Worker)
+
+    future = c.submit(inc, 1)
+    assert isinstance(future, Future)
+    assert future.key in c.futures
+
+    # result = future.result()  # This synchronous API call would block
+    result = await future
+    assert result == 2
+
+    assert future.key in s.tasks
+    assert future.key in a.data or future.key in b.data
