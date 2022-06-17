e1ad5797474255c752e0a26c5bc7f4d0c9b030ad
Chris Havlin <chris.havlin@gmail.com>
2022-06-17 16:24:29 -0500

    add pytest arguments for n workers, threads pw

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,11 +2,16 @@ import pytest
 from dask.distributed import Client, LocalCluster
 
 
+def pytest_addoption(parser):
+    parser.addoption("--dask_workers", action="store", default=2)
+    parser.addoption("--dask_threads_pw", action="store", default=1)
+
+
 # https://github.com/dask/distributed/issues/4479#issuecomment-772119435
 @pytest.fixture(scope="session")
-def dask_client_fixture():
-    n_workers = 2
-    n_threads_pw = 1
-    with LocalCluster(n_workers=n_workers, threads_per_worker=n_threads_pw) as cluster:
+def dask_client_fixture(request):
+    n_workers = int(request.config.getoption("--dask_workers"))
+    tpw = int(request.config.getoption("--dask_threads_pw"))
+    with LocalCluster(n_workers=n_workers, threads_per_worker=tpw) as cluster:
         with Client(cluster) as client:
             yield client
