afa6e701fc29e6f2a26a028bf30ce0697f186112
Chris Havlin <chris.havlin@gmail.com>
2023-01-10 11:16:42 -0600

    and set up display forwarding

--- ci_sandbox/magicgui_related/tests/test_magicgui.py
+++ ci_sandbox/magicgui_related/tests/test_magicgui.py
@@ -5,7 +5,7 @@ def test_default_widget():
     import sys
     from PyQt5 import QtWidgets
     QtWidgets.QApplication(sys.argv)
-    
+
     widget_class, opts = get_default_widget("x", 1.0, int)
     assert widget_class is SpinBox
 
