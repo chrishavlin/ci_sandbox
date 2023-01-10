16c8f7f71d099845aa382201c6a32e8b4e0e837a
Chris Havlin <chris.havlin@gmail.com>
2023-01-10 11:24:24 -0600

    add magicgui backend handling

--- ci_sandbox/magicgui_related/tests/test_magicgui.py
+++ ci_sandbox/magicgui_related/tests/test_magicgui.py
@@ -1,13 +1,20 @@
 from ci_sandbox.magicgui_related.container import get_default_widget
 from magicgui.widgets import SpinBox
+from magicgui import use_app
+import pytest
 
-def test_default_widget():
-    import sys
-    from PyQt5 import QtWidgets
-    QtWidgets.QApplication(sys.argv)
+# following magicgui testing approach, create a backend fixgure
+@pytest.fixture(scope="module", params=["ipynb", "qt"])
+def backend(request):
+    return request.param
+
+def test_default_widget(backend):
+    app = use_app(backend)
 
     widget_class, opts = get_default_widget("x", 1.0, int)
     assert widget_class is SpinBox
 
     c = widget_class(**opts)
     assert isinstance(c, SpinBox)
+
+    c.close()
