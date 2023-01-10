b69881b4d6dfa261e6f6c97a51ded71113eea442
Chris Havlin <chris.havlin@gmail.com>
2023-01-10 11:13:46 -0600

    try initializing widget within test

--- ci_sandbox/magicgui_related/tests/test_magicgui.py
+++ ci_sandbox/magicgui_related/tests/test_magicgui.py
@@ -2,6 +2,10 @@ from ci_sandbox.magicgui_related.container import get_default_widget
 from magicgui.widgets import SpinBox
 
 def test_default_widget():
+    import sys
+    from PyQt5 import QtWidgets
+    QtWidgets.QApplication(sys.argv)
+    
     widget_class, opts = get_default_widget("x", 1.0, int)
     assert widget_class is SpinBox
 
